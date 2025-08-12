233642adf9290b2ecbc43c51ac04555b370dfa2f
SSSensational <sss18201672034@163.com>
2025-08-12 15:00:33 +0800

    feat: AccountManagePlugin add split builds

--- packages/react-utils/tsup.config.ts
+++ packages/react-utils/tsup.config.ts
@@ -15,9 +15,40 @@ export const allUtilsEntry = readdirSync('./src').reduce(
   {} as Record<string, string>,
 );
 
+const accountPluginsEntry = (() => {
+  const entries: Record<string, string> = {};
+  
+  const walletPlugins = [
+    'use-wallet/Ethereum',
+    'use-wallet/MetaMask', 
+    'use-wallet/Coinbase',
+    'use-wallet/Fluent-Ethereum',
+    'use-wallet/Fluent-Conflux',
+    'use-wallet/Halo',
+    'use-wallet/OKX',
+    'use-wallet/TokenPocket'
+  ];
+  
+  walletPlugins.forEach(plugin => {
+    const pluginName = plugin.split('/')[1];
+    entries[`AccountManagePlugins/${pluginName}`] = `src/AccountManagePlugins/${plugin}.ts`;
+  });
+  
+  // 添加其他插件
+  entries['AccountManagePlugins/6963Wallet'] = 'src/AccountManagePlugins/6963Wallet/index.ts';
+  entries['AccountManagePlugins/wallet-connect'] = 'src/AccountManagePlugins/wallet-connect/index.ts';
+  
+  return entries;
+})();
+
 export default defineConfig({
-  entry: allUtilsEntry,
+  entry: {
+    ...allUtilsEntry,
+    ...accountPluginsEntry,
+  },
   format: 'esm',
   target: 'es2022',
   noExternal: ['@cfx-kit/utils', '@cfx-kit/dapp-utils'],
+  treeshake: true,
+  external: ['@cfxjs/use-wallet-react']
 });
